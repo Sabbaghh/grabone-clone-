306e93e2ee3b2fee87d6eecbce7748afe8acdc09
Sabbaghh <afsabbagh9@gmail.com>
2021-08-08 04:40:42 +0300

    Initialize dummy data

---
+++ src/data/headerData.js
@@ -0,0 +1,36 @@
+const headerData = [
+	{
+		id: 1,
+		image: './images/product1.jpg',
+		badge: './images/badge1.png',
+		title: 'Premium Dessert Package for Two',
+		brand: 'OKO Dessert & Bar',
+		location: 'Newmarket',
+		numOfBuyers: 25,
+		oldPrice: 56,
+		newPrice: 35,
+	},
+	{
+		id: 2,
+		image: './images/product1.jpg',
+		badge: './images/badge1.png',
+		title: 'Premium Dessert Package for Two',
+		brand: 'OKO Dessert & Bar',
+		location: 'Newmarket',
+		numOfBuyers: 25,
+		oldPrice: 56,
+		newPrice: 35,
+	},
+	{
+		id: 3,
+		image: './images/product1.jpg',
+		badge: './images/badge1.png',
+		title: 'Premium Dessert Package for Two',
+		brand: 'OKO Dessert & Bar',
+		location: 'Newmarket',
+		numOfBuyers: 25,
+		oldPrice: 56,
+		newPrice: 35,
+	},
+];
+export default headerData;
