1ca28a2ff6560c7a2d697d440e776b74cc17014b
Sabbaghh <afsabbagh9@gmail.com>
2021-08-10 00:51:43 +0300

    Creating Section component

---
+++ src/components/Section.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import headerData from '../data/headerData';
+import Card from './Card';
+const Section = ({ sectionName, cardData }) => {
+	return (
+		<section className='flex justify-center mt-2 flex-direction-column '>
+			<div className='flex p-1 section-header'>
+				<h3 className='color-secondary'>{sectionName}</h3>
+				<a href='#!' className='flex color-primary section-link align-center'>
+					View More
+					<span>&gt;</span>
+				</a>
+			</div>
+			<div className='flex flex-wrap justify-center w-full'>
+				{cardData.map((cardData) => {
+					return <Card key={cardData.id} cardData={cardData} />;
+				})}
+			</div>
+		</section>
+	);
+};
+
+export default Section;
