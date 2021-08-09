24420a8d04885550aaaecb6c911d529bc472728b
Sabbaghh <afsabbagh9@gmail.com>
2021-08-10 00:53:50 +0300

    Adding styles for diffrent nav bars

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,20 +1,30 @@
 import React from 'react';
 import '../styles/navbar.css';
+import BottomNav from './BottomNav';
 import DropDownList from './DropDownList';
+import InnerNav from './InnerNav';
+import SecondaryNav from './SecondaryNav';
 
 const Header = () => {
 	return (
-		<nav className='bg-primary flex space-between p-1 navbar'>
-			<div id='brand'>
-				<a href='#!'>
-					<img src='./images/brand.svg' alt='brand' />
-				</a>
-			</div>
-			<DropDownList
-				list={['Auckland', 'Waikato']}
-				icon='fas fa-map-marker-alt'
-			/>
-		</nav>
+		<>
+			<nav className='flex p-1 bg-primary space-between navbar'>
+				<div id='brand'>
+					<a href='#!'>
+						<img src='./images/brand.svg' alt='brand' />
+					</a>
+				</div>
+				<div className='flex'>
+					<DropDownList
+						list={['Auckland', 'Waikato']}
+						icon='fas fa-map-marker-alt'
+					/>
+					<InnerNav />
+					<BottomNav />
+				</div>
+			</nav>
+			<SecondaryNav />
+		</>
 	);
 };
 
