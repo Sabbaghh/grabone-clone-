5eb7260d53a1a2bb7e04393756d028115d55c5a7
Sabbaghh <afsabbagh9@gmail.com>
2021-08-10 00:52:55 +0300

    Making DropDownList more dynamic

--- src/components/DropDownList.js
+++ src/components/DropDownList.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import '../styles/dropDownList.css';
 
-const DropDownList = ({ list, icon }) => {
+const DropDownList = ({ list, icon, color }) => {
 	return (
-		<div className='dropDownList flex align-center text-white'>
-			<span className='icon'>
-				<i className={icon}></i>
-			</span>
-			<select>
-				{list.map((item, index) => {
+		<div className='flex text-white dropDownList align-center'>
+			{icon && (
+				<span className='icon'>
+					<i
+						className={icon}
+						style={color ? { color: color } : { color: '#FFF' }}
+					></i>
+				</span>
+			)}
+
+			<select
+				className='color-secondary'
+				style={color ? { color: color } : { color: '#FFF' }}
+			>
+				{list?.map((item, index) => {
 					return (
 						<option key={index} value={item}>
 							{item}
